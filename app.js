0fec0b4e9e2bff0e295e67b98e1c9164a2d4184b
Gerard Lozano <geriloza@gmail.com>
2022-01-31 12:36:26 +0100

    projects, puclications, conferences, news and job offers done

--- app.js
+++ app.js
@@ -66,7 +66,9 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
+//render all the dynamic components of the page
 function render(){
+  //Members rendering
   const memberContiner = document.getElementById("memberGroup")
   for(let member of members){
     memberContiner.innerHTML+=`
@@ -94,26 +96,135 @@ function render(){
                 ${member.description}
               </p>
             </div>
+            
             <div class="modal-footer">
-              <a href="${member.link}" target="_blank">More information</a>
+              ${member.link!==""?`<a href="${member.link}" target="_blank">More information</a>`:``}
             </div>
           </div>
         </div>
       </div>
     `
   }
+
+  //News rendering
+  const newsContainer = document.getElementById("news")
+  for(let noticia of news){
+    newsContainer.innerHTML+=`
+    <h4 class="member-name advisor">${noticia.name}</h4>
+    <p>${noticia.brief!==""?`${noticia.brief}<br>`:``}<a href="${noticia.link}" target="_blank">More information</a></p>
+    `
+  }
+
+  //Conferences rendering
+  const conferencesContainer = document.getElementById("conferencesContainer")
+  if(conferences.length>0){
+    conferencesContainer.style.display="flex"
+    conferencesContainer.innerHTML+=`
+    <h3>CONFERENCES</h3>
+    <div class="container" id="conferences">
+
+    </div>
+    `
+    const conferencesContent = document.getElementById("conferences")
+    for(let conference of conferences){
+      conferencesContent.innerHTML+=``
+    }
+  }
+  else{
+    conferencesContainer.style.display="none"
+  }
+
+  //Publications rendering
+  const publicationsContainer = document.getElementById("publicationsContainer")
+  if(publications.length>0){
+    publicationsContainer.style.display="flex"
+    publicationsContainer.innerHTML+=`
+    <h3>PUBLICATIONS</h3>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+
+    </div>
+    `
+    const publicationsContent = document.getElementById("accordionFlushExample")
+    for(let publication of publications){
+      publicationsContent.innerHTML+=`
+      <div class="accordion-item">
+        <h3 class="accordion-header" id="flush-headingOne">
+          <button style="border:1px solid black" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            ${publication.name}
+          </button>
+        </h3>
+        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">${publication.brief}</div>
+          <a class="accordion-body" target="_blank" href="${publication.link}">PUBLICATION LINK</a>
+        </div>
+      </div>
+      `
+    }
+  }
+  else{
+    publicationsContainer.style.display="none"
+  }
+  
+  //JobOffers rendering
+  const offersContainer = document.getElementById("jobContainer")
+  if(jobOffers.length>0){
+    offersContainer.style.display="flex"
+    offersContainer.innerHTML+=`
+    <h3>JOB OFFERS</h3>
+    <div class="container" id="jobs">
+
+    </div>
+    `
+    const jobsContent = document.getElementById("jobs")
+    for(let job of jobOffers){
+      jobsContent.innerHTML+=``
+    }
+  }
+  else{
+    offersContainer.style.display="none"
+  }
+
+  //Related Projects rendering
+  const projectsContainer = document.getElementById("projectsContainer")
+  for(let project of relatedProjects){
+    projectsContainer.innerHTML+=`
+    <div class="card w-75" style="margin-bottom:30px">
+      <div class="card-body">
+        <h5 class="card-title"><b>${project.name}</b></h5>
+        <p class="card-text">${project.brief}</p>
+        ${project.links.map(link=>`<a href="${link}" class="card-link">${link}</a>`)}
+      </div>
+    </div>
+    `
+    
+  }
+
+  //Advisory Board rendering
+  const advisoryContainer = document.getElementById("advisoryBoard")
+  for(let advisor of advisoryBoard){
+    advisoryContainer.innerHTML+=`
+    <h4 class="member-name advisor">${advisor.name}</h4>
+    <p><b>${advisor.location}</b> <br> ${advisor.brief}<br><a href="${advisor.link}" target="_blank">More information</a></p>
+    `
+  }
+
+  //Authorities rendering
   const authoritiesContainer = document.getElementById("authoritiesGroup")
   for(let i=1;i<6;i++){
     authoritiesContainer.innerHTML+=`
     <img class="institutionImage" src="./images/authority${i}.png" alt="authority image">
     `
   }
+
+  //Universities rendering
   const universitiesContainer = document.getElementById("universitiesGroup")
   for(let i=1;i<5;i++){
     universitiesContainer.innerHTML+=`
     <img class="institutionImage" src="./images/university${i}.png" alt="university image">
     `
   }
+
+  //Companies rendering
   const companiesContainer = document.getElementById("companiesGroup")
   const othersContainer = document.getElementById("othersGroup")
   for(let i=1;i<4;i++){
--- dynamicObjects.js
+++ dynamicObjects.js
@@ -1,38 +1,39 @@
 const members = [
+    //id, name, brief, description, link
     {
         id : 1,
         name : "Dr. Sara Rodríguez Mozaz",
         brief: "Research Scientist, ICRA, PI of ReUseMP3",
         description:"Dr. Sara Rodriguez-Mozaz focuses her research on the development of advanced analytical tools to study the origin, fate, and ecological impact of the so-called emerging contaminants in the environment and in human health. She is PI of ReUseMP3 and leader of WP3, WP4, and WP5.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=55940978800"
     },
     {
         id : 2,
         name : "Dr. Gianluigi Buttiglieri",
         brief: "Research Scientist, ICRA, PI of ReUseMP3",
         description:"Dr. Gianluigi Buttiglieri focuses his research on water reuse, circular economy, and nature-based solutions, with a special attention on the fate of organic micropollutants in water and wastewater technologies. He is PI of ReUseMP3 and leader of WP1, WP2 and WP4.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=15031298100"
     },
     {
         id : 3,
         name : "Dr. Quim Comas",
         brief: "Professor, UdG",
         description:"Dr. Quim Comas is expert on the development of tools and technologies to foster the implementation of circular economy in the urban water cycle, multicriteria decision support systems, nature-based solutions, and membrane technologies. In ReUseMP3 he will be working mainly in WP2 and WP6.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=56484784800"
     },
     {
         id : 4,
         name : "Dr. Eusebi Calle Ortega",
         brief: "Professor, UdG",
         description:"Dr. Eusebi Calle Ortega has expertise in the design and analysis of availability, robustness, and routing schemes in different types of networks, including water distribution networks. In ReUseMP3 he will be working mainly in WP6.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=7005657673"
     },
     {
         id : 5,
         name : "Dr. Diana Alvarez",
         brief: "SCT-UEM-LC responsible, ICRA",
         description:"Dr. Diana Alvarez is responsible of ICRA SCT-UEM-LC. She is expert on the development and application of analytical methodologies for the identification and quantification of emerging pollutants and biomarkers in environmental samples. In ReUseMP3 she will be working mainly in WP3.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=6503937806"
     },
     {
         id : 6,
@@ -46,20 +47,106 @@ const members = [
         name : "Esther Mendoza",
         brief: "PhD student, ICRA",
         description:"Esther Mendoza is MSc in Environmental Sciences, and she is working in her PhD on circular economy to facilitate water reuse in touristic cities. She has expertise on treatment technologies, including forward and hydroponic technologies. In ReUseMP3 she will be working mainly in WP1 and WP2.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=57214114107"
     },
     {
         id : 8,
         name : "Marc Castaño",
         brief: "PhD student, ICRA",
         description:"Marc Castaño is MSc in Chromatography, and he is working in his PhD on tracking micropollutants in water loops. He has expertise on the identification and quantification of emerging pollutants in environmental samples. In ReUseMP3 he will be working mainly in WP3, WP4, and WP5.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=57210104126"
     },
     {
         id : 9,
         name : "Dr. Gaetan Blandin",
         brief: "Postdoctoral Researcher, UdG",
         description:"Dr. Gaetan Blandin focuses on water processes associated with membrane technologies, in particular forward osmosis technology, within the context of circular economy. Currently at Lequia working of FORWARD Factory project (La Caixa Junior leader fellow). In ReUseMP3 he will be working mainly in WP1.",
-        link: ""
+        link: "https://www.scopus.com/authid/detail.uri?authorId=56625035000"
     }
+]
+
+const advisoryBoard=[
+    //name, location, brief, link
+    {
+        name : "Evina Katsou",
+        location:"Brunel University (UK)",
+        brief:"Risk assessment, decision support systems and micropollutants",
+        link:"https://www.scopus.com/authid/detail.uri?authorId=26040737500"
+    },
+    {
+        name : "Monica Brienza",
+        location:"Basilicata University (Italy)",
+        brief:"Water reuse and micropollutants",
+        link:"https://www.scopus.com/authid/detail.uri?authorId=56367434500"
+    },
+    {
+        name : "Natasa Atanasova",
+        location:"Ljubljana University (Slovenia)",
+        brief:"Co-chair of the Circular City COST, NBS and decision support systems",
+        link:"https://www.scopus.com/authid/detail.uri?authorId=12763953800"
+    },
+    {
+        name : "Pedro Carvalho",
+        location:"Aarhus University (Denmark)",
+        brief:"NBS and micropollutants",
+        link:"https://www.scopus.com/authid/detail.uri?authorId=22978130100"
+    },
+    {
+        name : "Xavier Amores",
+        location:"(Girona, Spain)",
+        brief:"Cluster Manager of the Catalan Water Partnership",
+        link:"http://www.cwp.cat/en/"
+    },
+]
+
+const publications=[
+    //name, brief, link
+]
+
+const conferences=[
+    //name,brief,link
+]
+
+
+const jobOffers=[
+    //name,brief,link
+]
+
+const news=[
+    //name,brief,link
+    {
+        name:"ICRA newsletter September 2021",
+        brief:"",
+        link:"https://mailchi.mp/a1811b7d3d04/icra-news-setembre-2021?e=b546b9fa1f"
+    },
+    {
+        name:"RETEMA",
+        brief:"Nature-based solutions for the transition to circular cities",
+        link:"https://www.retema.es/noticia/soluciones-basadas-en-la-naturaleza-para-la-transicion-hacia-ciudades-circulares-R2cY"
+    }
+]
+
+const relatedProjects=[
+    //name,brief,links
+    {
+        name:"HYDROUSA",
+        brief:"Demonstration of water loops with innovative regenerative business models for the Mediterranean region",
+        links:["https://www.hydrousa.org/","https://www.icra.cat/projects/demonstration-water-loops-with-innovative/122"]
+    },
+    {
+        name:"MULTISOURCE",
+        brief:"ModULar Tools for Integrating enhanced natural treatment SOlutions into Urban wateR CyclEs",
+        links:["https://multisource.eu/","https://www.icra.cat/projects/multisource-modular-tools-integrating-enhanced/181"]
+    },
+    {
+        name:"LIFE-RECYCLO",
+        brief:"recycling waste water from small and mèdium sized laundries with advances Oxidation process",
+        links:["http://treewater.fr/fr/recyclo","https://www.icra.cat/projectes/life-recyclo-recycling-waste-water/202"]
+    },
+    {
+        name:"SAFE",
+        brief:"Sustainable water reuse practices improving Safety in Agriculture, Food and Environment",
+        links:[]
+    }
+
 ]
