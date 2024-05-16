67836461f8193ded8277ee75338f0b35adc2800e
Oriol <o.camposlara@gmail.com>
2024-05-16 11:06:36 +0200

    Update webpage

--- JavaScript/email.js
+++
@@ -1,8 +0,0 @@
-import * as fs from "node:fs";
-
-export default function send() {
-    let email = document.getElementById("inputEmail").value
-
-    fs.appendFileSync('./emails/emails.txt',email);
-    console.log(email)
-}
